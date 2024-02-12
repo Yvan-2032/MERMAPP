a9cb2b5445340bfe51594207719f24f0ace6cde6
Yvan Bautista <yvan2032@gmail.com>
2024-02-12 16:34:34 -0500

    Clase 4
    Configuraciones del Backend

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
 const URL = process.env.MONGODB_URL
+  ? process.env.MONGODB_URL
+  : 'mongodb://localhost/databasetest'
 mongoose.connect(URL)
 
 const connection = mongoose.connection
